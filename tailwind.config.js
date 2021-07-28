8e1045e717b050658c6fdf0aed23e9b9725103e9
avizmarques <aviz.marques@gmail.com>
2021-07-28 12:52:24 +0200

    style updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
       display: ["SF Pro Display", "sans-serif"],
-    }, 
+    },
     fontSize: {
       sm: ["14px", "17px"],
-      base: ["16px", "24px"],
+      base: ["16px", "19px"],
       lg: ["20px", "28px"],
-      xl: ["34px", "41px"],
+      xl: ["24px", "28px"],
     },
     colors: {
+      "darkest-gray": "#1F2933",
       "dark-gray": "#323F4B",
       "light-gray": "#DBE0E6",
-      "gray": "#9AA5B1",
-      "transparent": "transparent"
+      gray: "#9AA5B1",
+      transparent: "transparent",
     },
-    extend: {
-    },
+    extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
